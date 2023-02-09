2e8d72548e72d6d3162c28180346ce1cf37e9976
Daniil Suvorov <syvorov.daniil@gmail.com>
2023-02-09 11:35:49 +0700

    add Vector Tests

--- test/LibTest.cs
+++ test/LibTest.cs
@@ -1,6 +1,5 @@
 namespace test;
 using lib;
-using vector;
 public class LibTest
 {
     [Fact]
@@ -18,58 +17,5 @@ public class LibTest
         bool actual = TestLib.AreIntersected(arg);
 
         Assert.Equal(expected, actual);
-    }
-    [Fact]
-    public void VectorEqualTest()
-    {
-        Vector v1 = new Vector(2, 0, -2);
-        Vector v2 = new Vector(2, 0, -2);
-        bool actual = v1 == v2;
-        bool expected = true;
-        Assert.Equal(actual, expected);
-    }
-    [Fact]
-    public void VectorNotEqualTest()
-    {
-        Vector v1 = new Vector(0, 0, 0);
-        Vector v2 = new Vector(0, 0, -2);
-        bool actual = v1 == v2;
-        bool expected = true;
-        Assert.NotEqual(actual, expected);
-    }
-    [Fact]
-    public void VectorTwoDotsEqualTest()
-    {
-        Vector v1 = new Vector(0, 10, -4, 0, 9, -5);
-        Vector v2 = new Vector(0, 5, -2, 0, 4, -3);
-        bool actual = v1 == v2;
-        bool expected = true;
-        Assert.Equal(actual, expected);
-    }
-    [Fact]
-    public void VectorNormaTest()
-    {
-        Vector v1 = new Vector(4, 0, 3);
-        double actual = v1.norma();
-        double expected = 5;
-        Assert.Equal(actual, expected);
-    }
-    [Fact]
-    public void VectorCos180Test()
-    {
-        Vector v1 = new Vector(4, 0, 3);
-        Vector v2 = new Vector(-4, 0, -3);
-        double actual = v1.cos(v2);
-        double expected = -1;
-        Assert.Equal(actual, expected);
-    }
-    [Fact]
-    public void VectorCos0Test()
-    {
-        Vector v1 = new Vector(4, 9, 3);
-        Vector v2 = new Vector(20, 45, 15);
-        double actual = v1.cos(v2);
-        double expected = 1;
-        Assert.Equal(actual, expected);
     }
 }
